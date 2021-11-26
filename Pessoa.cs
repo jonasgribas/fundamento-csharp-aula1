81706aa27c1c0233bdbe844d3401e3c431e291c2
jonasgribas <jonas-g-ribas@hotmail.com>
2021-11-26 21:12:59 +0000

    Exercicio2

---
+++ Pessoa.cs
@@ -0,0 +1,12 @@
+class Pessoa
+{
+    public int idade;
+    public string nome;
+    public int peso;
+
+    public Pessoa(int pIdade, string pNome)
+    {
+        idade = pIdade;
+        nome = pNome;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace dotnetcore
 {
     class Program
     {
-        // Exercício 1:
-        // - Fazer a média de notas dos dois alunos (Zuqui, Bronza) e imprimir no Console.WriteLine();
-        // - Imprimir no console para cada aluno
-        //      - Nome do aluno
-        //      - Caso média maior ou igual 7:
-        //          - Passou no ano letivo
-        //          - Não passou no ano letivo
-        // Requisítos:
-        // - Criar um método "CalcularMedia", onde a entrada de parâmetros seja as notas e o retorno seja a média em float
-        // - Utilizar For ou Foreach
-
-
         // Exercício 2:
         // - Fazer um programa que cadastre pessoas, contendo nome e idade;
         // - Imprimir a media de idade das pessoas
@@ -29,40 +18,27 @@ namespace dotnetcore
         static void Main(string[] args)
         {
             Console.WriteLine("Iniciando Programa");
-            
-            var xNotasZuqui = new List<int>() { 7, 6, 5, 3 }; //Lista
-            var xNotasBronza = new List<int>() { 8, 9, 10, 8 }; //Lista
-            var xMediaZ = CalcularMedia(xNotasZuqui);
-            var xMediaB = CalcularMedia(xNotasBronza);
 
-            if (xMediaZ >= 7)
-            {
-                Console.WriteLine("Zuqui");
-                Console.WriteLine("Passou no ano letivo");
-            }else{
-                Console.WriteLine("Não passou no ano letivo");
-            }
+        var xPessoas = new List<Pessoa>();
+        xPessoas.Add(new Pessoa(31,"Jonas"));
+        xPessoas.Add(new Pessoa(32,"Eduardo"));
 
-            if (xMediaB >= 7)
-            {
-                Console.WriteLine("Bronza");
-                Console.WriteLine("Passou no ano letivo");
-            }else{
-                Console.WriteLine("Não passou no ano letivo");
-            }      
+        // - Imprimir a media de idade das pessoas
+        var xMedia = xPessoas.Select(p => p).Average(p => p.idade);
+        Console.WriteLine(xMedia);
+        
+        // - Imprimir o nome e idade da pessoa mais velha
+        var xVelha = xPessoas.OrderByDescending(p => p.idade).FirstOrDefault();
+        Console.WriteLine(xVelha.nome + ": " + xVelha.idade);
+
+        // - Imprimir o nome e idade da pessoa mais nova
+        var xNova = xPessoas.OrderBy(p => p.idade).FirstOrDefault();
+        Console.WriteLine(xNova.nome + ": " + xNova.idade);
+
+        // - Imprimir a quantidade de pessoas com maior idade
+        var xQtdeIdade = xPessoas.Where(p => p.idade>=18).Count();
+        Console.WriteLine("Existem " + xQtdeIdade + " pessoas maiores de idade");
         }      
-         public static float CalcularMedia(List<int> pNotas)
-          {
-              var xSoma = 0.0f;
-              
-              foreach (var xNota in pNotas)
-              {
-                  xSoma = xSoma + xNota;             
-              }
-
-              var xMedia = xSoma/pNotas.Count; 
-              
-              return xMedia;
-          }
+         
     }
 }
